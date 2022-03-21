29938ca73d3e34bb61ef5714c311a21583f6f871
Samuel2779 <samuelcueto.2000@gmail.com>
2022-03-21 17:12:41 -0600

    Added cart view

--- store-frontend/src/App.tsx
+++ store-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CartView from "./views/CartView";
 import HomepageView from "./views/HomepageView";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
@@ -7,6 +8,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/cart" element={<CartView />} />
         <Route path="/login" element={<LoginView />} />
         <Route path="/register" element={<RegisterView />} />
         <Route path="/" element={<HomepageView />} />
