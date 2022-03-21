7e163e517d7a911e96a48478d32f8af99f712a37
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2022-03-21 04:22:41 -0600

    Single product page

--- store-frontend/src/App.tsx
+++ store-frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomepageView from "./views/HomepageView";
+import Product from "./views/Product";
+import ProductList from "./views/ProductList";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" />
-        <Route path="/" element={<HomepageView />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/login" />
+    //     <Route path="/" element={<HomepageView />} />
+    //   </Routes>
+    // </BrowserRouter>
+    //<ProductList/>
+    <Product/>
   );
 };
 
--- store-frontend/src/views/ProductList.tsx
+++ store-frontend/src/views/ProductList.tsx
@@ -1,6 +1,33 @@
 import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
 
 const Container = styled.div`
+
+`
+const Title = styled.h1`
+    margin: 20px;
+`
+const FilterContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Filter = styled.div`
+    margin: 20px;
+`
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`
+const Option = styled.option`
 
 `
 
@@ -8,7 +35,46 @@ const Container = styled.div`
 const ProductList: React.FC = () => {
     return (
       <Container>
-       
+          <Navbar/>
+          <Announcement/>
+            <Title>PC PARTS</Title>
+          <FilterContainer>
+                <Filter> 
+                    <FilterText>Filter Products</FilterText>
+                    <Select>
+                        <Option disabled selected>
+                            Color
+                        </Option>
+                        <Option>RGB</Option>
+                        <Option>Neutrals</Option>
+                        <Option>Pastel Colours</Option>
+
+                    </Select>
+                    <Select>
+                        <Option disabled selected>
+                            Type
+                        </Option>
+                        <Option> PC Parts</Option>
+                        <Option>PC Cases</Option>
+                        <Option>PC Add-Ons</Option>
+
+                    </Select>
+                    </Filter>
+                <Filter>  
+                    <FilterText>Sort Products</FilterText>
+                    <Select>
+                        <Option  selected> Type </Option>
+                        <Option> Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                        
+
+                    </Select>
+                    </Filter>
+          </FilterContainer>
+
+          <Products/>
+          <Newsletter/>
+          <Footer/>
       </Container>
     );
   };
