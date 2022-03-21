e40f1fb284d2011fd89b75497855ab72aabbbd98
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2022-03-21 01:01:39 -0600

    HomePage View

--- store-frontend/src/views/HomepageView.tsx
+++ store-frontend/src/views/HomepageView.tsx
@@ -1,10 +1,13 @@
+import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const HomepageView: React.FC = () => {
   return (
     <div>
-      <h1>Homepage View</h1>
+      <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
