3487b64b2e06235ab5cc8e42ebb4620878700ffe
Manuel Herrasti <42578763+wozsax@users.noreply.github.com>
2022-03-23 22:17:55 -0600

    Login auth

--- store-frontend/src/redux/store.ts
+++ store-frontend/src/redux/store.ts
@@ -1,9 +1,44 @@
-import {configureStore} from "@reduxjs/toolkit"
+import {configureStore, combineReducers} from "@reduxjs/toolkit"
 import cartReducer from "./cartRedux"
+import userReducer from "./userRedux"
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+  import storage from 'redux-persist/lib/storage'
+  
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  const rootReducer = combineReducers({user:userReducer,cart: cartReducer});
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default configureStore({
+  
+export const store: any = () => configureStore({
     reducer:{
         cart: cartReducer,
+        user: persistedReducer as any,
         
-    }
-})
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+
+
+
+export let persistor = persistStore(store);
+export default store;
--- store-frontend/src/views/LoginView.tsx
+++ store-frontend/src/views/LoginView.tsx
@@ -1,5 +1,9 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { login } from "../redux/apiCalls";
 import { mobails } from "../responsive";
+
 
 const Container = styled.div`
   width: 110v;
@@ -45,6 +49,10 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled{
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 const Link = styled.a`
   margin: 5px 0px;
@@ -52,18 +60,35 @@ const Link = styled.a`
   text-decoration: underline;
   cursor: pointer;
 `;
+const Error = styled.span`
+  color: red;
+`;
 
 const LoginView: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const {isFetching, error } = useSelector((state: any) => state.user)
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>)=>{
+    e.preventDefault()
+    login(dispatch,{username, password});
+  }
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username"></Input>
+          <Input placeholder="username"
+           
+           onChange={(e)=>setUsername(e.target.value)}>
+           
 
-          <Input placeholder="password"></Input>
+           </Input>
 
-          <Button>LOGIN</Button>
+          <Input placeholder="password" type="password" onChange={(e)=>setPassword(e.target.value)}></Input>
+
+          <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+          {error &&<Error>Something went wrong...</Error>}
           <Link>FORGOT PASSWORD?</Link>
           <Link>CREATE NEW ACCOUNT</Link>
         </Form>
