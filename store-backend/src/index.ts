ca377fb9270c9d738c8743062c6e37405f42c06e
Lars17S <efren.garibaldi@gmail.com>
2022-05-13 21:24:33 -0500

    cors origin bug fixed

--- store-backend/src/index.ts
+++ store-backend/src/index.ts
@@ -24,7 +24,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3002"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
